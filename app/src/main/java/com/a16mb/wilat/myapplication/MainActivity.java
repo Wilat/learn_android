fa964d82a342262f6f1c5d9f3c6078de09d0e140
Wilat <Wilat@mail.ru>
2016-10-08 12:38:36 +0500

    test android commit

---
+++ app/src/main/java/com/a16mb/wilat/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.a16mb.wilat.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void buttonSendOnClick() {
+        //EditText sendedMessage = new EditText(R.id.sendedMessage));
+        //String message = sendedMessage.toString();
+    }
+}
